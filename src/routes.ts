4b07079c38edea394eca8e3e459a4e61d53c8907
Cali93 <armut.cumaali@gmail.com>
2021-11-12 02:39:18 +0100

    updated join route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 export const routes = {
   memepalooza:
     'https://gitcoin.notion.site/Welcome-To-Memepalooza-s-Home-Page-63f1146cf4994f5cb8a0a27106472aa5',
-  join: 'https://gitcoin.notion.site/Memes-Merch-Marketing-c1bd89fac8ab4569bf2a676df28025d8',
+  join: 'https://form.typeform.com/to/YuA2wHkg',
   request: 'https://form.typeform.com/to/iM5lAFFD',
   home: 'https://gitcoin.notion.site/Memes-Merch-Marketing-c1bd89fac8ab4569bf2a676df28025d8',
 };
