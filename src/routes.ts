e7b27bbd39635ab8db7c87e5b210765f7e910867
Cali93 <armut.cumaali@gmail.com>
2021-11-22 18:53:17 +0100

    new notion homepage

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,6 @@ export const routes = {
     'https://gitcoin.notion.site/Welcome-To-Memepalooza-s-Home-Page-63f1146cf4994f5cb8a0a27106472aa5',
   join: 'https://form.typeform.com/to/YuA2wHkg',
   request: 'https://form.typeform.com/to/iM5lAFFD',
-  home: 'https://gitcoin.notion.site/Memes-Merch-Marketing-c1bd89fac8ab4569bf2a676df28025d8',
+  home: 'https://gitcoin.notion.site/Memes-Merch-Marketing-60cc693f0fe14be1879a05587922c8a3',
   weekly: 'https://meet.google.com/ffs-ubtu-nfi',
 };
